797df5f6ed4a012d85b23bbfeac8a1de182f1bdc
philliplumod <philliplumod@gmail.com>
2023-02-02 05:53:15 +0800

    quotes

---
+++ quoteapp/lib/main/quote_list.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class QouteList extends StatefulWidget {
+  const QouteList({super.key});
+
+  @override
+  State<QouteList> createState() => _QouteListState();
+}
+
+class _QouteListState extends State<QouteList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
